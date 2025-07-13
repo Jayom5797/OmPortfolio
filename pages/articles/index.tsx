8ba0ee69d9983e0b0dca57b4b260dd53aa9fa7de
Jayom5797 <jayom5797@gmail.com>
2025-07-13 16:57:38 +0530

    Fix: Removed unused ServerProps to pass ESLint

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import { GetStaticProps } from 'next';
 import PortfolioLayout from '../../components/Portfolio/PortfolioLayout/PortfolioLayout';
-import { wrapper } from '../../store';
-import { bindActionCreators, Dispatch } from 'redux';
-import { loadLatestArticles } from '../../store/action-creators/articles-action-creators';
-import { connect } from 'react-redux';
-import AllDevToArticles from '../../components/Portfolio/AllDevToArticles/AllDevToArticles';
-
-interface ServerProps {
-  title: string;
-}
+import Navbar from '../../components/Portfolio/Navbar/Navbar';
 
 /**
  *Renders articles page with the list of latest dev.to articles
@@ -20,31 +11,44 @@ interface ServerProps {
  *@returns {JSX.Element} - Rendered Clock component
  */
 
-const ArticlesPage: NextPage<ServerProps> = ({ title }) => {
+const CADModelsPage: NextPage = () => {
   return (
-    <PortfolioLayout title={title}>
-      <AllDevToArticles />
+    <PortfolioLayout title="My CAD Models">
+      <Navbar isLogoExpanded={true} />
+      <div
+        style={{
+          minHeight: '60vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <h1
+          style={{
+            color: '#2bff88',
+            fontSize: '2.5rem',
+            marginBottom: '1.5rem',
+          }}
+        >
+          My CAD Models
+        </h1>
+        <p
+          style={{
+            color: '#fff',
+            fontSize: '1.25rem',
+            textAlign: 'center',
+            maxWidth: 600,
+          }}
+        >
+          Here you will soon find a showcase of my CAD (Computer-Aided Design)
+          models and 3D projects.
+          <br />
+          Stay tuned for updates!
+        </p>
+      </div>
     </PortfolioLayout>
   );
 };
 
-export const getStaticProps: GetStaticProps = wrapper.getStaticProps(
-  (store) => async () => {
-    await store.dispatch(loadLatestArticles());
-
-    return {
-      props: {
-        title: 'Om Singh | Articles',
-      },
-      revalidate: 3600,
-    };
-  }
-);
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    news: bindActionCreators(loadLatestArticles, dispatch),
-  };
-};
-
-export default connect(null, mapDispatchToProps)(ArticlesPage);
+export default CADModelsPage;
